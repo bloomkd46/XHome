81523413eb6dc28a7ff8bd7203fd2d9589c2efc7
Kolton Bloom <bloomkd46@icloud.com>
2022-12-19 16:30:08 -0800

    Added Router type (2)

---
+++ src/devices/Router.ts
@@ -0,0 +1,44 @@
+import { AxiosInstance } from 'axios';
+
+
+export class Router {
+  public onchange?: (oldState: RouterDevice, newState: RouterDevice) => void;
+
+  constructor(protected server: AxiosInstance, public device: RouterDevice) {
+    //
+  }
+
+  async get(): Promise<RouterDevice> {
+    const device: RouterDevice = (await this.server.get(this.device._links.self.href)).data;
+    if (this.onchange && (JSON.stringify(device) !== JSON.stringify(this.device))) {
+      this.onchange(this.device, device);
+    }
+    this.device = device;
+    return this.device;
+  }
+}
+export interface RouterDevice {
+  firmwareVersion: string;
+  hardwareId: string;
+  manufacturer: 'Netgear';
+  macAddress: string;
+  status: 'online';
+  technology: 'wifi';
+  _links: {
+    self: { href: string; };
+  };
+  model: 'WNR1000' | string;
+  deletable: boolean;
+  deviceType: 'peripheral';
+  name: 'Network Peripheral';
+  properties: { type: 'router'; label: 'Network Peripheral'; };
+  icontrolModel: 'WNR1000' | string;
+  id: string;
+  deviceSubtype: 'default';
+  trouble: {
+    description: string;
+    name: 'senTamp' | string;
+    criticality: boolean;
+    timestamp: number;
+  }[];
+}
